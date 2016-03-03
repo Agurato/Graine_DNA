93995a484e368e6b5c624b68bdda1afd2977da69
Baalon <filipegama@laposte.net>
2016-03-03 23:15:07 +0100

    completed test for EarStrand

--- mainStrandTest.cpp
+++ mainStrandTest.cpp
@@ -16,14 +16,14 @@ using namespace std;
 int main() {
 	srand(time(0));
 
-	vector<bool> location = decToBinary(2, 4);
-	vector<bool> location2 = decToBinary(3, 4);
+	vector<bool> location = decToBinary(4, 4);
+	vector<bool> location2 = decToBinary(7, 4);
 	location.insert(location.end(), location2.begin(), location2.end());
 	int i;
 
 	/* Ears Test */
 
-	cout << "First Ear" << endl;
+	cout << "First Ear : RANDOM MODE" << endl;
 	EarStrand first_earStrand(4);
 
 	int first_earStrandNb = first_earStrand.getNumber();
@@ -36,8 +36,16 @@ int main() {
 		cout << "First Ear getLocationOf(" << i << ") :" << binaryToString(first_earStrand.getLocationOf(i)) << endl;
 	}
 
-	cout << "Second Ear" << endl;
-//	EarStrand second_earStrand(2, location);
+
+	cout << "Second Ear : SET MODE" << endl;
+	EarStrand second_earStrand(2, location);
+
+	cout << "Second Ear Sequence : " << binaryToString(second_earStrand.getSequence()) << endl;
+	cout << "Second Ear getNumber() : " << second_earStrand.getNumber() << endl;
+	cout << "Second Ear getNumberBin() : " << binaryToString(second_earStrand.getNumberBin()) << endl;
+	cout << "Second Ear getLocations() : " <<  binaryToString(second_earStrand.getLocations()) << endl;
+	cout << "Second Ear getLocationOf(0) :" << binaryToString(second_earStrand.getLocationOf(0)) << endl;
+	cout << "Second Ear getLocationOf(1) :" << binaryToString(second_earStrand.getLocationOf(1)) << endl;
 
 
 	return 0;
