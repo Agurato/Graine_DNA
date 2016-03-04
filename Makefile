4bbced767924045bb5c3b990f1844e1887581539
Baalon <filipegama@laposte.net>
2016-03-04 23:09:50 +0100

    Finished Creature DNA test : it works perfectly.

--- Makefile
+++ Makefile
@@ -8,7 +8,7 @@ mainEarStrandTest :
 	g++ mainEarStrandTest.cpp utils/utils.cpp dna_strands/EarStrand.cpp -o mainEarStrandTest.out -std=c++11
 
 mainCreatureTest :
-	g++ mainCreatureTest.cpp utils/utils.cpp parts/XMLData.cpp dna_strands/Strand.cpp dna_strands/EarStrand.cpp DNA.cpp Creature.cpp -o mainCreatureTest.out -std=c++11 -lxerces-c
+	g++ mainCreatureTest.cpp utils/utils.cpp parts/XMLData.cpp dna_strands/Strand.cpp dna_strands/EarStrand.cpp dna_strands/EyeStrand.cpp DNA.cpp Creature.cpp -o mainCreatureTest.out -std=c++11 -lxerces-c
 
 clean :
 	rm mainStrandTest.out parser.out utilsTest.out mainCreatureTest.out
--- mainCreatureTest.cpp
+++ mainCreatureTest.cpp
@@ -24,10 +24,12 @@ int main() {
 
 	srand(time(0));
 
-	vector<bool> location = decToBinary(4, 4);
-	vector<bool> location2 = decToBinary(7, 4);
+	vector<bool> location = decToBinary(3, xml->LIMB_NB_LENGTH());
+	vector<bool> location2 = decToBinary(1, xml->LIMB_NB_LENGTH());
 	location.insert(location.end(), location2.begin(), location2.end());
 
+
+/* Ear Strand */
 	cout << "First Ear" << endl;
 	EarStrand first_earStrand(2, location);
 
@@ -40,22 +42,61 @@ int main() {
 
 	cout << "___________________________________" << endl;
 
+/* Eye Strand */
 
+	vector<bool> eyeColour = hexToBinary("FFFFFF");
+
+	location = decToBinary(2, xml->LIMB_NB_LENGTH());
+	location2 = decToBinary(14, xml->LIMB_NB_LENGTH());
+	vector<bool> location3 = decToBinary(1, xml->LIMB_NB_LENGTH());
+	location.insert(location.end(), location2.begin(), location2.end());
+	location.insert(location.end(), location3.begin(), location3.end());
+
+	cout << "First Eye" << endl;
+	EyeStrand first_eyeStrand(eyeColour, 3, location);
+
+	cout << "First Eye Sequence : " << binaryToString(first_eyeStrand.getSequence()) << endl;
+	cout << "First Eye getNumber() : " << first_eyeStrand.getNumber() << endl;
+	cout << "First Eye getNumberBin() : " << binaryToString(first_eyeStrand.getNumberBin()) << endl;
+	cout << "First Eye getLocations() : " <<  binaryToString(first_eyeStrand.getLocations()) << endl;
+	cout << "First Eye getLocationOf(0) :" << binaryToString(first_eyeStrand.getLocationOf(0)) << endl;
+	cout << "First Eye getLocationOf(1) :" << binaryToString(first_eyeStrand.getLocationOf(1)) << endl;
+	cout << "First Eye getLocationOf(2) :" << binaryToString(first_eyeStrand.getLocationOf(2)) << endl;
+	cout << "First Eye getColor() :" << first_eyeStrand.getColor() << endl;
+	cout << "First Eye getColorBin() :" << binaryToString(first_eyeStrand.getColorBin()) << endl;
+
+	cout << "___________________________________" << endl;
+
+/* Creature */
 	DNA dna1;
 	dna1.addStrand(&first_earStrand);
+	dna1.addStrand(&first_eyeStrand);
 
 	Creature creature1(0, dna1);
 	DNA dna12 = creature1.getDNA();
 	vector<Strand*> vectorStrand1 = dna12.getDNAStrand();
 	EarStrand* first_creature_earStrand = (EarStrand*) vectorStrand1[0];
+	EyeStrand* first_creature_eyeStrand = (EyeStrand*) vectorStrand1[1];
 
 	cout << "First Creature ID : " << creature1.getID() << endl;
+	cout << "-------------------------" << endl;
 	cout << "First Creature Ear Sequence : " << binaryToString(first_creature_earStrand->getSequence()) << endl;
 	cout << "First Creature Ear getNumber() : " << first_creature_earStrand->getNumber() << endl;
 	cout << "First Creature Ear getNumberBin() : " << binaryToString(first_creature_earStrand->getNumberBin()) << endl;
 	cout << "First Creature Ear getLocations() : " <<  binaryToString(first_creature_earStrand->getLocations()) << endl;
 	cout << "First Creature Ear getLocationOf(0) :" << binaryToString(first_creature_earStrand->getLocationOf(0)) << endl;
 	cout << "First Creature Ear getLocationOf(1) :" << binaryToString(first_creature_earStrand->getLocationOf(1)) << endl;
+	cout << "----------------------------" << endl;
+	cout << "First Creature Eye Sequence : " << binaryToString(first_creature_eyeStrand->getSequence()) << endl;
+	cout << "First Creature Eye getNumber() : " << first_creature_eyeStrand->getNumber() << endl;
+	cout << "First Creature Eye getNumberBin() : " << binaryToString(first_creature_eyeStrand->getNumberBin()) << endl;
+	cout << "First Creature Eye getLocations() : " <<  binaryToString(first_creature_eyeStrand->getLocations()) << endl;
+	cout << "First Creature Eye getLocationOf(0) :" << binaryToString(first_creature_eyeStrand->getLocationOf(0)) << endl;
+	cout << "First Creature Eye getLocationOf(1) :" << binaryToString(first_creature_eyeStrand->getLocationOf(1)) << endl;
+	cout << "First Creature Eye getLocationOf(2) :" << binaryToString(first_creature_eyeStrand->getLocationOf(2)) << endl;
+	cout << "First Creature Eye getColor() :" << first_creature_eyeStrand->getColor() << endl;
+	cout << "First Creature Eye getColorBin() :" << binaryToString(first_creature_eyeStrand->getColorBin()) << endl;
+	cout << "----------------------------" << endl;
 
 
 	return 0;
